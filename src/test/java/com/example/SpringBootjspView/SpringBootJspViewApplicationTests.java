21d87bc724feda8f907606681d3e86f0842d1630
Yash Omar <49341731+yashomar@users.noreply.github.com>
2021-03-25 11:02:24 +0530

    Add files via upload

---
+++ src/test/java/com/example/SpringBootjspView/SpringBootJspViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootjspView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
